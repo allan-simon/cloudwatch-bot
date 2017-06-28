08e1283368373b2d254cad9f323199663a9287d4
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2017-06-28 18:22:01 +0200

    Refactor MessageType enum

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@
 ///////////////////
 
 #[macro_use]
+extern crate lazy_static;
+#[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
--- src/sns/errors.rs
+++ src/sns/errors.rs
@@ -1,3 +1,19 @@
 use serde_json;
+use std::collections::HashMap;
+use std::fmt::{Display, Error, Formatter};
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub(crate) struct ParseEnumError<V> {
+    pub value: String,
+    pub mapping: HashMap<&'static str, V>,
+}
+
+impl<V> Display for ParseEnumError<V> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        let keys: Vec<&'static str> = self.mapping.keys().map(|v| v.clone()).collect();
+        write!(fmt, "'{}' does not match allowed enum values: {}", self.value, keys.join(" | "))
+    }
+}
+
+pub(crate) type EnumResult<T> = Result<T, ParseEnumError<T>>;
 pub(crate) type JsonResult<T> = Result<T, serde_json::Error>;
